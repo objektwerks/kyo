44087cf071521c0c78d7db35b5407a87568894d2
objektwerks <objektwerks@runbox.com>
2023-09-29 04:42:04 -0400

    edited effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -6,7 +6,7 @@ import org.scalatest.matchers.should.Matchers
 import kyo.*
 
 class EffectTest extends AnyFunSuite with Matchers:
-  test(">") {
+  test("empty") {
     val a: Int > Any = 1
     a.pure shouldBe 1
   }
