bf8caa6f15c047045da4d65447740e8477b77bdb
objektwerks <objektwerks@runbox.com>
2023-09-29 04:50:17 -0400

    enhanced effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -7,7 +7,9 @@ import kyo.*
 import kyo.options.Options
 import kyo.tries.Tries
 
-class EffectTest extends AnyFunSuite with Matchers:
+import scala.util.Success
+
+final class EffectTest extends AnyFunSuite with Matchers:
   test("empty") {
     val i: Int > Any = 1
     i.pure shouldBe 1
@@ -17,5 +19,6 @@ class EffectTest extends AnyFunSuite with Matchers:
     val i: Int > Any = 1
     val io: Int > Options = i
     val iot: Int > (Options with Tries) = io
-    
-  }
+    Options.run(io) shouldBe Some(1)
+    Tries.run(iot) shouldBe Success(1)
+  } 
