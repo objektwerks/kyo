d47fdb3d829cd16e4a4bf852d8c596f783c9edbe
objektwerks <objektwerks@runbox.com>
2023-09-29 04:37:47 -0400

    enhanced effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -3,7 +3,10 @@ package objektwerks
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import kyo.*
+
 class EffectTest extends AnyFunSuite with Matchers:
   test(">") {
-    
+    val a: Int > Any = 1
+    a.pure shouldBe 1
   }
