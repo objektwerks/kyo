3c95c4b9595ecb71629624825e8efab5e9eba3a6
objektwerks <objektwerks@runbox.com>
2023-09-29 04:50:47 -0400

    reformatted effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -10,15 +10,13 @@ import kyo.tries.Tries
 import scala.util.Success
 
 final class EffectTest extends AnyFunSuite with Matchers:
-  test("empty") {
+  test("empty"):
     val i: Int > Any = 1
     i.pure shouldBe 1
-  }
 
-  test("widening") {
+  test("widening"):
     val i: Int > Any = 1
     val io: Int > Options = i
     val iot: Int > (Options with Tries) = io
     Options.run(io) shouldBe Some(1)
     Tries.run(iot) shouldBe Success(1)
-  } 
