6123d8cb162716a5c84a9861b9aeae8069a4366d
objektwerks <objektwerks@runbox.com>
2023-09-30 14:27:32 -0400

    added fiber test

--- src/test/scala/objektwerks/ConcurrentEffectTest.scala
+++ src/test/scala/objektwerks/ConcurrentEffectTest.scala
@@ -4,6 +4,8 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import kyo.*
+import kyo.concurrent.fibers.{Fiber, Fibers}
+import kyo.ios.IOs
 
 import scala.annotation.tailrec
 
@@ -13,6 +15,6 @@ final class ConcurrentEffectTest extends AnyFunSuite with Matchers:
     case i if i < 1 => acc
     case _ => factorial(n - 1, acc * n)
 
-  test("todo"):
-    val i: Int > Any = 1
-    i.pure shouldBe 1
+  test("fiber"):
+    val fiber: Fiber[Int] > (Fibers with IOs) = Fibers.forkFiber(factorial(9))
+    
