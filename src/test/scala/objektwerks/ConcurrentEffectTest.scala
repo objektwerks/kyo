8572aa0b3f8e0be4cf78b502da34d656c126b774
objektwerks <objektwerks@runbox.com>
2023-09-30 13:08:36 -0400

    renamed test

--- src/test/scala/objektwerks/ConcurrentEffectTest.scala
+++ src/test/scala/objektwerks/ConcurrentEffectTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.matchers.should.Matchers
 
 import kyo.*
 
-final class FiberTest extends AnyFunSuite with Matchers:
+final class ConcurrentEffectTest extends AnyFunSuite with Matchers:
   test("todo"):
     val i: Int > Any = 1
     i.pure shouldBe 1
