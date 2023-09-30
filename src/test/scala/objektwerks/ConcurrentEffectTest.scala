357b0d9a62396bcd7b27f3b84ade728f77298186
objektwerks <objektwerks@runbox.com>
2023-09-30 14:25:24 -0400

    added factorial to concurrent effect test

--- src/test/scala/objektwerks/ConcurrentEffectTest.scala
+++ src/test/scala/objektwerks/ConcurrentEffectTest.scala
@@ -5,7 +5,14 @@ import org.scalatest.matchers.should.Matchers
 
 import kyo.*
 
+import scala.annotation.tailrec
+
 final class ConcurrentEffectTest extends AnyFunSuite with Matchers:
+  @tailrec
+  def factorial(n: Int, acc: Int = 1): Int = n match
+    case i if i < 1 => acc
+    case _ => factorial(n - 1, acc * n)
+
   test("todo"):
     val i: Int > Any = 1
     i.pure shouldBe 1
