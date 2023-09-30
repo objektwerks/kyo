fa6e90fd45ab2b4297378f49a98f2bbfa9bec742
objektwerks <objektwerks@runbox.com>
2023-09-30 13:03:32 -0400

    added fiber test

---
+++ src/test/scala/objektwerks/FiberTest.scala
@@ -0,0 +1,11 @@
+package objektwerks
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import kyo.*
+
+final class FiberTest extends AnyFunSuite with Matchers:
+  test("todo"):
+    val i: Int > Any = 1
+    i.pure shouldBe 1
