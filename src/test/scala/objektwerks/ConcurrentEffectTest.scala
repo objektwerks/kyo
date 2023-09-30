cd427f111dd0ae40ab1c91baebd4f57faa2c1e1f
objektwerks <objektwerks@runbox.com>
2023-09-30 17:03:36 -0400

    removed logger from concurrent effect test

--- src/test/scala/objektwerks/ConcurrentEffectTest.scala
+++ src/test/scala/objektwerks/ConcurrentEffectTest.scala
@@ -5,15 +5,11 @@ import org.scalatest.matchers.should.Matchers
 
 import kyo.*
 import kyo.concurrent.fibers.{Fiber, Fibers}
-import kyo.direct.*
 import kyo.ios.IOs
-import kyo.loggers.{Logger, Loggers}
 
 import scala.annotation.tailrec
 
 final class ConcurrentEffectTest extends AnyFunSuite with Matchers:
-  val logger: Logger = Loggers.init(getClass())
-
   @tailrec
   def factorial(n: Int, acc: Int = 1): Int = n match
     case i if i < 1 => acc
