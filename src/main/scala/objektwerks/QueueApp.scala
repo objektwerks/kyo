f8e4d6e984fd62ec94028e6b72404d1e33d1c96f
objektwerks <objektwerks@runbox.com>
2023-10-02 15:03:08 -0400

    added queue app

---
+++ src/main/scala/objektwerks/QueueApp.scala
@@ -0,0 +1,21 @@
+package objektwerks
+
+import kyo.*
+import kyo.concurrent.queues.{Queue, Queues}
+import kyo.consoles.Consoles
+import kyo.ios.IOs
+import kyo.loggers.Loggers
+
+object QueueApp extends App:
+  val logger = Loggers.init(getClass())
+  val queue: Queue[Int] > IOs = Queues.bounded(capacity = 3)
+  
+  def run(args: List[String]) = 
+    for
+      _         <- Consoles.println("*** Enter a number:")
+      candidate <- Consoles.readln
+      number    =  candidate.toIntOption.getOrElse(1)
+      queued    <- queue.map(_.offer(number))
+      _         <- Consoles.println(s"*** Queued: $number")
+      _         <- logger.info(s"*** Queued: $number")
+    yield ()
