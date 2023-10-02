2e1e5d77077a703689dbfe9bc2465e10b1939e00
objektwerks <objektwerks@runbox.com>
2023-10-02 14:43:33 -0400

    removed common

--- src/main/scala/objektwerks/Common.scala
+++
@@ -1,8 +0,0 @@
-package objektwerks
-
-import scala.annotation.tailrec
-
-@tailrec
-def factorial(n: Int, acc: Int = 1): Int = n match
-  case i if i < 1 => acc
-  case _ => factorial(n - 1, acc * n)
