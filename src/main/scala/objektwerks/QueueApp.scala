d4014bded699ff298ddfecc51718b1fde5d7868d
objektwerks <objektwerks@runbox.com>
2023-10-02 15:18:24 -0400

    refactored queue app

--- src/main/scala/objektwerks/QueueApp.scala
+++ src/main/scala/objektwerks/QueueApp.scala
@@ -1,23 +1,22 @@
 package objektwerks
 
 import kyo.*
-import kyo.concurrent.queues.{Queue, Queues}
+import kyo.concurrent.queues.Queues
 import kyo.consoles.Consoles
-import kyo.ios.IOs
 import kyo.loggers.Loggers
 
 object QueueApp extends App:
   val logger = Loggers.init(getClass())
-  val queue: Queue[Int] > IOs = Queues.bounded(capacity = 3)
   
-  def run(args: List[String]) = 
+  def run(args: List[String]) =
     for
+      queue     <- Queues.bounded[Int](capacity = 3)
       _         <- Consoles.println("*** Enter a number:")
       candidate <- Consoles.readln
       number    =  candidate.toIntOption.getOrElse(1)
-      offer     <- queue.map(_.offer(number))
+      offer     <- queue.offer(number)
       _         <- Consoles.println(s"*** Offer of $number succeeded: $offer")
-      poll      <- queue.map(_.poll)
+      poll      <- queue.poll
       _         <- Consoles.println(s"*** Polled: ${poll.getOrElse(-1)}")
       _         <- logger.info(s"*** Polled: ${poll.getOrElse(-1)}")
     yield ()
