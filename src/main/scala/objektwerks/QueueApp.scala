8d874280d4eff3b3695d404e6dbd7f66de7eb322
objektwerks <objektwerks@runbox.com>
2023-11-21 13:55:11 -0500

    corrected api change in queue app

--- src/main/scala/objektwerks/QueueApp.scala
+++ src/main/scala/objektwerks/QueueApp.scala
@@ -10,7 +10,7 @@ object QueueApp extends App:
   
   def run(args: List[String]) =
     for
-      queue     <- Queues.initBounded[Int](capacity = 1)
+      queue     <- Queues.init[Int](capacity = 1)
       _         <- Consoles.println("*** Enter a number:")
       candidate <- Consoles.readln
       number    =  candidate.toIntOption.getOrElse(1)
