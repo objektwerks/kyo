cfabed1c52d088e70a722d6f984bf0601f186183
objektwerks <objektwerks@runbox.com>
2024-08-29 15:42:21 -0400

    enhanced effect test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -4,4 +4,6 @@ import munit.FunSuite
 
 final class EffectTest extends FunSuite:
   test("todo"):
-    println("*** The version 0.11.0 upgrade destroyed this test!")
+    val message = "*** The version 0.11.0 upgrade destroyed this test!"
+    println(message)
+    assert(message.nonEmpty)
