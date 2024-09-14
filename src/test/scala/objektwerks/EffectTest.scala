09a08479e5c4c49b5d5bd2b140edbb36af4dc5c9
objektwerks <objektwerks@runbox.com>
2024-09-14 14:22:19 -0400

    added test

--- src/test/scala/objektwerks/EffectTest.scala
+++ src/test/scala/objektwerks/EffectTest.scala
@@ -13,3 +13,10 @@ final class EffectTest extends FunSuite:
     val i: Int < Any = 1
     val j: Int = i.eval
     assertEquals(j, 1)
+
+  test("effect"):
+    def double(i: Int): Int < Abort[Exception] = i * i
+
+    val result: Result[Exception, Int] < Any = Abort.run(double(2))
+    val answer: Int = result.eval.getOrElse(0)
+    assertEquals(answer, 4)
