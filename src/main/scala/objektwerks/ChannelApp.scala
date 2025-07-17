12035e558354919c7ef1e328ed3fe92d9869c186
objektwerks <objektwerks@runbox.com>
2025-07-17 08:49:40 -0400

    corrected channel app

--- src/main/scala/objektwerks/ChannelApp.scala
+++ src/main/scala/objektwerks/ChannelApp.scala
@@ -6,13 +6,13 @@ object ChannelApp extends KyoApp:
   run:
     for
       channel   <- Channel.init[Int](capacity = 1)
-      _         <- Console.println("*** Enter a number:")
-      candidate <- Console.readln
+      _         <- Console.print("*** Enter a number:")
+      candidate <- Console.readLine
       number    =  candidate.toIntOption.getOrElse(1)
       _         <- channel.put(number)
-      _         <- Console.println(s"*** Put number: $number")
+      _         <- Console.print(s"*** Put number: $number")
       _         <- Log.info(s"*** Put number: $number")
       take      <- channel.take
-      _         <- Console.println(s"*** Take number: $take")
+      _         <- Console.print(s"*** Take number: $take")
       _         <- Log.info(s"*** Take number: $take")
     yield ()
