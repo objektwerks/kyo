8c6a5c59980069b1f2ee58039bf60831c63a4ffd
objektwerks <objektwerks@runbox.com>
2025-07-17 08:50:45 -0400

    corrected queue app

--- src/main/scala/objektwerks/QueueApp.scala
+++ src/main/scala/objektwerks/QueueApp.scala
@@ -6,13 +6,13 @@ object QueueApp extends KyoApp:
   run:
     for
       queue     <- Queue.init[Int](capacity = 1)
-      _         <- Console.println("*** Enter a number:")
-      candidate <- Console.readln
+      _         <- Console.print("*** Enter a number:")
+      candidate <- Console.readLine
       number    =  candidate.toIntOption.getOrElse(1)
       offer     <- queue.offer(number)
-      _         <- Console.println(s"*** Offer of $number succeeded: $offer")
+      _         <- Console.print(s"*** Offer of $number succeeded: $offer")
       _         <- Log.info(s"*** Offer of $number succeeded: $offer")
       poll      <- queue.poll
-      _         <- Console.println(s"*** Polled: ${poll.getOrElse(-1)}")
+      _         <- Console.print(s"*** Polled: ${poll.getOrElse(-1)}")
       _         <- Log.info(s"*** Polled: ${poll.getOrElse(-1)}")
     yield ()
