3271c7bf47e4e0f09afdc6d85a80eebfa63a91d5
objektwerks <objektwerks@runbox.com>
2025-07-17 08:50:11 -0400

    corrected console app

--- src/main/scala/objektwerks/ConsoleApp.scala
+++ src/main/scala/objektwerks/ConsoleApp.scala
@@ -5,11 +5,11 @@ import kyo.*
 object ConsoleApp extends KyoApp:
   run:
     for
-      _        <- Console.println("*** What is your name?")
-      name     <- Console.readln
-      _        <- Console.println(s"*** Greetings, $name!")
+      _        <- Console.print("*** What is your name?")
+      name     <- Console.readLine
+      _        <- Console.print(s"*** Greetings, $name!")
       _        <- Log.info(s"*** Greetings, $name!")
       datetime <- Clock.now
-      _        <- Console.println(s"*** The current date and time is: $datetime")
+      _        <- Console.print(s"*** The current date and time is: $datetime")
       _        <- Log.info(s"*** The current date and time is: $datetime")
     yield ()
