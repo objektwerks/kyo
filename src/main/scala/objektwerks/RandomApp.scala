9c7fa879796caaef05955a4892c03ef6697e6afb
objektwerks <objektwerks@runbox.com>
2025-07-17 08:52:01 -0400

    corrected random app

--- src/main/scala/objektwerks/RandomApp.scala
+++ src/main/scala/objektwerks/RandomApp.scala
@@ -5,13 +5,13 @@ import kyo.*
 object RandomApp extends KyoApp:
   run:
     for
-      a        <- Random.nextInt(11)
-      _        <- Console.println(s"*** Random number: $a")
-      b        <- Random.nextInt(11)
-      _        <- Console.println(s"*** Random number: $b")
-      c        <- Random.nextInt(11)
-      _        <- Console.println(s"*** Random number: $c")
-      t        = a + b + c
-      _        <- Console.println(s"*** Random numbers ($a + $b + $c) total: $t!")
-      _        <- Log.info(s"*** Random numbers ($a + $b + $c) total: $t!")
+      a <- Random.nextInt(11)
+      _ <- Console.print(s"*** Random number: $a")
+      b <- Random.nextInt(11)
+      _ <- Console.print(s"*** Random number: $b")
+      c <- Random.nextInt(11)
+      _ <- Console.print(s"*** Random number: $c")
+      t = a + b + c
+      _ <- Console.print(s"*** Random numbers ($a + $b + $c) total: $t!")
+      _ <- Log.info(s"*** Random numbers ($a + $b + $c) total: $t!")
     yield ()
